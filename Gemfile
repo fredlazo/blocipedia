c52a234d05a60af19ce945347083c20507c7663d
Frederick Lazo <fredlazo@yahoo.com>
2016-07-26 20:30:30 -0700

    Checkpoint-6 completed

--- Gemfile
+++ Gemfile
@@ -22,6 +22,8 @@ source 'https://rubygems.org'
 
  gem 'pundit'
 
+ gem 'faker'
+
  # Use SCSS for stylesheets
  gem 'sass-rails', '~> 5.0'
  # Use Uglifier as compressor for JavaScript assets
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+# Create some users
+3.times do
+	user = User.new(
+		email:		Faker::Internet.email,
+		password:	Faker::Internet.password(10)
+	)
+	user.skip_confirmation!
+	user.save!
+end
+users = User.all
+
+15.times do
+	Wiki.create!(
+		user:	users.sample,
+		title:	Faker::Lorem.sentence,
+    body: Faker::Lorem.paragraphs(1)
+
+	)
+end
+wikis = Wiki.all
+
+user = User.first
+user.skip_reconfirmation!
+user.update_attributes!(
+	email: 'fredlazo@yahoo.com',
+	password: 'password'
+)
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Wiki.count} wikis created"
