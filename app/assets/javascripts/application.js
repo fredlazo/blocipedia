e51160c14ee1da8b5dd4d4b7496c2807a67e13fb
Frederick Lazo <fredlazo@yahoo.com>
2016-08-19 14:51:27 -0700

    sdfkl

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,7 +22,7 @@ var blocmetrics = {};
 blocmetrics.report = function(eventName) {
   var event = {event: { name: eventName}};
   var request = new XMLHttpRequest();
-  request.open("POST", "https://tranquil-badlands-49839.herokuapp.com/api/events", true);
+  request.open("POST", "http://localhost:3000/api/events", true);
   request.setRequestHeader('Content-Type', 'application/json');
   request.send(JSON.stringify(event));
 };
